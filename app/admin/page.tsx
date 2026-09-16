4f5c404a1c216374564ce6628a57d3699fea704e
S R <s.regtuijt+git@gmail.com>
2026-09-16 07:57:52 +0200

    Put admin importer in separate tab

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,9 @@
 import { redirect } from 'next/navigation'
-import ImportClient from './import-client'
 import { getAdminEmail } from '@/lib/admin'
-import ArtworkEditor from './artwork-editor'
+import AdminDashboard from './admin-dashboard'
 
 export default async function AdminPage() {
   const email = await getAdminEmail()
   if (!email) redirect('/login?callbackUrl=%2Fadmin')
-  return <><ImportClient /><ArtworkEditor /></>
+  return <AdminDashboard />
 }
